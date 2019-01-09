a32ef1d4da7ec01158e00b3af0a7650d00529a65
Pavel Glukhovskov <glukhovskov@adwz.ru>
2019-01-09 17:46:39 +0300

    Build

--- build/image-gallery.js
+++ build/image-gallery.js
@@ -94,12 +94,15 @@ var ImageGallery = function (_React$Component) {
         if (this.props.onSlide) {
           this.props.onSlide(this.state.currentIndex);
         }
-
-        this._updateThumbnailTranslate(prevState);
+        if (this.props.showThumbnails) {
+          this._updateThumbnailTranslate(prevState);
+        }
       }
 
       if (prevState.currentThumbnailIndex !== this.state.currentThumbnailIndex) {
-        this._updateThumbnailTranslate(prevState);
+        if (this.props.showThumbnails) {
+          this._updateThumbnailTranslate(prevState);
+        }
       }
 
       if (prevProps.slideDuration !== this.props.slideDuration) {
@@ -566,7 +569,7 @@ var ImageGallery = function (_React$Component) {
         return this._thumbnails.scrollHeight - thumbnailsWrapperHeight;
       }
 
-      return this._thumbnails.scrollWidth - thumbnailsWrapperWidth;
+      return this._thumbnails ? this._thumbnails.scrollWidth - thumbnailsWrapperWidth : 0;
     }
   }, {
     key: '_setThumbsTranslate',
